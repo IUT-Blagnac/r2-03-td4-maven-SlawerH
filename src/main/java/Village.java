4acc71ca775309ca5b5730ca71b6d7c001806f7d
Slawer <slawer.hugo@gmail.com>
2022-02-25 14:15:51 +0000

    feat: javadoc

--- src/main/java/Village.java
+++ src/main/java/Village.java
@@ -2,11 +2,12 @@ import java.util.ArrayList;
 
 public class Village {
 	private String nom;
-	// private Schtroumpf[] schtroumpfs;
-	// private int nbSchtroumpfs;
 	private ArrayList<Schtroumpf> schtroumpfs;
 	private int nbSalsepareille;
 	
+	/**
+	 * Crée un village
+	 */
 	public Village (String pfNomV, int pfStockSalsepareille, String[] pfNomsDesSchtroumpfs) {
 		this.nom = pfNomV;
 		this.nbSalsepareille = pfStockSalsepareille;
@@ -17,22 +18,36 @@ public class Village {
 			schtroumpf.setVillage(this);
 			
 			this.schtroumpfs.add(schtroumpf);
-			//this.nbSchtroumpfs++;
 		}
 	}
 	
+    /**
+     * Retourne le nom du village
+	 * @return le nom du village
+     */
 	public String getNom() {
 		return this.nom;
 	}
 	
+    /**
+     * Retourne le nombre de Schtroumpfs
+	 * @return le nombre de Schtroumpfs
+     */
 	public int getNbSchtroumpf() {
 		return this.schtroumpfs.size();
 	}
 	
+    /**
+     * Retourne le stock de salespareille
+	 * @return le stock de salespareille
+     */
 	public int getStocketSalsepareille() {
 		return this.nbSalsepareille;
 	}
 	
+	/**
+	 * Réalise un solstice d'été (tout les Schtroumpfs du village chantent et se présentent)
+	 */
 	public void solstice_d_ete() {
 		int nbContents = 0;
 		
@@ -53,6 +68,10 @@ public class Village {
 		System.out.println("Nombre de Schtroumpfs contents : " + nbContents);
 	}
 	
+	/**
+	 * Retourne le chef du village (plus âgé)
+	 * @return le chef du village
+	 */
 	public Schtroumpf chefDuVillage() {
 		int nbSchtroumpfs = this.schtroumpfs.size();
 		
@@ -71,6 +90,9 @@ public class Village {
 		return plusVieux;
 	}
 	
+	/**
+	 * Envoi les Schtroumpfs heureux (1 sur 2) au travail
+	 */
 	public void envoyerAuTravail() {
 		boolean prochainVaTravailler = false;
 
@@ -89,6 +111,9 @@ public class Village {
 		}
 	}
 	
+	/**
+	 * Réalise la présentation et le chant des Schtroumpfs heuruex
+	 */
 	public void schtroumpfsHeureux() {
 		int nbSchtroumpfs = this.schtroumpfs.size();
 		
@@ -103,10 +128,17 @@ public class Village {
 		}
 	}
 	
+	/**
+	 * Rajoute du stock de salsepareille
+	 * @param int pfQuantite : quantité à rajouter
+	 */
 	public void addStockSalsepareilles(int pfQuantite) {
 		this.nbSalsepareille += pfQuantite;
 	}
 	
+	/**
+	 * Réalisation du dîner des Schtroumpfs ensemble
+	 */
 	public void dinerTousEnsemble() {
 		int nbSchtroumpfs = this.schtroumpfs.size();
 		
@@ -115,6 +147,9 @@ public class Village {
 		}
 	}
 	
+	/**
+	 * Envoie 1 Schtroumpf sur 2 heureux à la récole
+	 */
 	public void envoyerCueillirSalsepareille() {
 		boolean prochainVaCueillir = false;
 
